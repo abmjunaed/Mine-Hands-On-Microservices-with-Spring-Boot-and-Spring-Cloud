13a06081e420d4b9d3f4a238a31141bf8324ac62
Junaed <abm.junaed@gmail.com>
2020-10-14 23:41:34 +0200

    generated projects using spring CLI, added multiproject settings.gradle

---
+++ microservices/product-composite-service/src/test/java/se/junaed/microservices/composite/product/ProductCompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.junaed.microservices.composite.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
