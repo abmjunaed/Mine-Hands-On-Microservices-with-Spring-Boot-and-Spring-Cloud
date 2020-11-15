c53d72d537c51bd6c8b12bc50cc19104f4482e27
Junaed <abm.junaed@gmail.com>
2020-11-15 11:18:47 +0100

    -added chap 03 files

--- chap-03/2-basic-rest-services/microservices/product-service/src/main/java/se/junaed/microservices/core/product/ProductServiceApplication.java
+++ chap-03/2-basic-rest-services/microservices/product-service/src/main/java/se/junaed/microservices/core/product/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package se.junaed.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("se.junaed")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
---
+++ chap-03/2-basic-rest-services/microservices/product-service/src/main/java/se/junaed/microservices/core/product/services/ProduceService.java
@@ -0,0 +1,5 @@
+package se.junaed.microservices.core.product.services;
+
+public class ProduceService {
+
+}
